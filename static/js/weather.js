8f7c1c13886f086f036a8b4f7ef8ef5b6d2d3a33
berniesamson <berniesamson@users.noreply.github.com>
2014-11-02 18:05:21 -0800

    update of edits so far. stuck on doge text

--- static/js/weather.js
+++ static/js/weather.js
@@ -1,3 +1,5 @@
+// Need to figure out how to get the image of text to change with temperature
+
 $(function ()
 {
 	$.get("http://ipinfo.io", function(response)
@@ -9,13 +11,19 @@ $(function ()
 		{
 			var description = data.weather[0].description;
 			var icon = "static/img/" + data.weather[0].icon + ".jpg";
+			var weathertext = "static/words/" + data.weather[0].icon + ".png";
 			var celcius = Math.round(data.main.temp - 273.15);
 			var fahrenheit = Math.round(celcius * 9/5 + 32);
 
 			$("h2").text(description);
-			$("body").css("backgroundImage", "url(" + icon + ")");
+			$("body").css( "backgroundImage", "url(" + icon + ")");
+			$("body").css( ".weathertext", "url(" + weathertext + ")");
 			$("#f").text(fahrenheit);
 			$("#c").text(celcius);
+
+			var doge_img = "url(./img/words/" + data.weather[0].icon + ".png)";
+
+
 		});
 
 	}, "jsonp");
